e620a922b7d43f048b839859dd9ae5ec8c694b1c
Régis EFF <86823724+Nefer-Hotep@users.noreply.github.com>
2022-06-22 17:36:06 +0200

    fix: nettoyage de commentaire

--- front/js/Controller.js
+++ front/js/Controller.js
@@ -15,41 +15,36 @@ class Controller {
   async kanapDisplay() {
     // Créer une variable de l'emplacement actuel de l'Url(search la partie après le ?)
     const urlSearch = new URLSearchParams(window.location.search);
-
     // Créer une constante Get qui lit l'id
     const id = urlSearch.get("id");
 
     let model = new Model();
     let kanap = await model.getKanap(id);
-    // console.log(kanap);
 
     let kanapView = new KanapView();
     kanapView.render(kanap);
-    // console.log(kanapView);
   }
 
   // Ajoute un produit aux panier depuis le onclick:"controller.addItemToCart()"
   addItemToCart() {
-    // Créer une variable de l'emplacement actuel de l'Url(search la partie après le ?)
     const urlSearch = new URLSearchParams(window.location.search);
-
-    // Créer une constante Get qui lit l'id
     const id = urlSearch.get("id");
+    let selectedQuantity = document.getElementById("quantity");
+    let selectedColors = document.getElementById("colors");
+    let quantity = +selectedQuantity.value;
+    let color = selectedColors.value;
 
-    // Créer une variable vide de l'input quantity
-    let selectedQuantity = document.getElementById("quantity")
-    let selectedColors = document.getElementById("colors")
-    
-    let quantity = +selectedQuantity.value
-    let color = selectedColors.value
-
-    let product = {}
-    product.id = id
-    product.quantity = quantity
-    product.color = color
+    let product = {};
+    product.id = id;
+    product.quantity = quantity;
+    product.color = color;
 
     let cart = new Cart(id, quantity, color);
-    cart.add(product)
+    cart.add(product);
     cart.save();
+
+    if (quantity == 0) {
+      alert("Vous n'avez pas sélectionnez un nombre d'article");
+    }
   }
 }
